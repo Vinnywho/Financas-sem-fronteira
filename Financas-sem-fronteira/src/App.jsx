03dbc11259edc00b3fb996e9f87db635a4154a55
Vinicius Lima <vivini13235@gmail.com>
2026-03-02 23:42:44 -0300

    [Fix] Corrigindo import

--- Financas-sem-fronteira/src/App.jsx
+++ Financas-sem-fronteira/src/App.jsx
@@ -2,7 +2,7 @@ import "./App.css";
 import Navbar from "./components/navbar/Navbar.jsx";
 import Home from "./components/home/Home.jsx";
 import Solucoes from "./components/solucoes/Solucoes.jsx";
-import Blog from "./components/Blog-outro/Blog.jsx";
+import Blog from "./components/blog-outro/Blog.jsx";
 import Proposito from "./components/proposito/Proposito.jsx";
 import Contato from "./components/contato/Contato.jsx";
 import Footer from "./components/footer/Footer.jsx";
