cfd611ff4dcbdaaeb433b88c5e74701df6ebb909
Vinicius Lima <vivini13235@gmail.com>
2026-03-06 17:47:35 -0300

    [Fix] Tentativa de correção navbar

--- Financas-sem-fronteira/src/components/navbar/Navbar.jsx
+++ Financas-sem-fronteira/src/components/navbar/Navbar.jsx
@@ -6,72 +6,71 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className={styles.nav}>
-      <a
-        href="#home"
-        className={styles.logo}
-      >
-        <img src={logo} alt="Logo" className={styles.logo} />
-      </a>
-
-      <button
-        className={`${styles.burger} ${isOpen ? styles.open : ""}`}
-        onClick={() => setIsOpen(!isOpen)}
-      >
-        <span></span>
-        <span></span>
-        <span></span>
-      </button>
-
-      <ul className={`${styles.navUl} ${isOpen ? styles.active : ""}`}>
-        <li>
-          <a
-            href="#home"
-            className={styles.navItem}
-            onClick={() => setIsOpen(false)}
-          >
-            HOME
-          </a>
-        </li>
-        <li>
-          <a
-            href="#proposito"
-            className={styles.navItem}
-            onClick={() => setIsOpen(false)}
-          >
-            PROPOSITO
-          </a>
-        </li>
-        <li>
-          <a
-            href="#solucoes"
-            className={styles.navItem}
-            onClick={() => setIsOpen(false)}
-          >
-            SOLUÇÕES
-          </a>
-        </li>
-        <li>
-          <a
-            href="#blog"
-            className={styles.navItem}
-            onClick={() => setIsOpen(false)}
-          >
-            BLOG
-          </a>
-        </li>
-        {/* <li><a href="#galeria" className={styles.navItem} onClick={() => setIsOpen(false)}>GALERIA</a></li> */}
-        {/* <li><a href="#depoimentos" className={styles.navItem} onClick={() => setIsOpen(false)}>DEPOIMENTOS</a></li> */}
-      </ul>
-
-      <div className={styles.navActions}>
-        <a href="#contato">
-          <button className={styles.contatoBtn}>
-            ENTRE EM CONTATO <span className={styles.seta}>→</span>
-          </button>
+    <div className={styles.navContainer}>
+      <nav className={styles.nav}>
+        <a href="#home" className={styles.logo}>
+          <img src={logo} alt="Logo" className={styles.logo} />
         </a>
-      </div>
-    </nav>
+
+        <button
+          className={`${styles.burger} ${isOpen ? styles.open : ""}`}
+          onClick={() => setIsOpen(!isOpen)}
+        >
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+
+        <ul className={`${styles.navUl} ${isOpen ? styles.active : ""}`}>
+          <li>
+            <a
+              href="#home"
+              className={styles.navItem}
+              onClick={() => setIsOpen(false)}
+            >
+              HOME
+            </a>
+          </li>
+          <li>
+            <a
+              href="#proposito"
+              className={styles.navItem}
+              onClick={() => setIsOpen(false)}
+            >
+              PROPOSITO
+            </a>
+          </li>
+          <li>
+            <a
+              href="#solucoes"
+              className={styles.navItem}
+              onClick={() => setIsOpen(false)}
+            >
+              SOLUÇÕES
+            </a>
+          </li>
+          <li>
+            <a
+              href="#blog"
+              className={styles.navItem}
+              onClick={() => setIsOpen(false)}
+            >
+              BLOG
+            </a>
+          </li>
+          {/* <li><a href="#galeria" className={styles.navItem} onClick={() => setIsOpen(false)}>GALERIA</a></li> */}
+          {/* <li><a href="#depoimentos" className={styles.navItem} onClick={() => setIsOpen(false)}>DEPOIMENTOS</a></li> */}
+        </ul>
+
+        <div className={styles.navActions}>
+          <a href="#contato">
+            <button className={styles.contatoBtn}>
+              ENTRE EM CONTATO <span className={styles.seta}>→</span>
+            </button>
+          </a>
+        </div>
+      </nav>
+    </div>
   );
 };
 
